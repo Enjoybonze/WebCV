5a3c11c3b9309b333d9bae89aa38f03582278e06
Enjoybonze <59249457+Enjoybonze@users.noreply.github.com>
2020-03-12 16:06:15 +0000

    123

--- Part3/javascript.js
+++ Part3/javascript.js
@@ -1,9 +1,9 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+    var x = document.getElementById("id");
     if (x.className === "topnav") {
       x.className += "responsive";
     } 
     else {
       x.className = "topnav";
     }
-  }
+}
