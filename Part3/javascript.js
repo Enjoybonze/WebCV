4c4cdb2fca0e9e167b1d604891bab76ad88b7ba2
Enjoybonze <59249457+Enjoybonze@users.noreply.github.com>
2020-04-01 11:41:53 +0100

    323

--- Part3/javascript.js
+++ Part3/javascript.js
@@ -11,3 +11,16 @@ function myFunction() {
 function myFunction() {
   document.getElementById("colorchange").style.color = "red";
 }
+
+
+
+function select (button) {
+  console.log(button.id)
+  document.getElementById("Frame").src ="./" + button.id + ".html";
+  let buttons = document.getElementsByClassName("buttonnav");
+  for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
+    element.classList.remove("selectedbutton");
+  }
+  button.classList.add("selectedbutton");
+}
